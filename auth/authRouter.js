4670427df76e3774fcf2a8956f48514306840f97
vidyakin <vidyakin.sergey@gmail.com>
2020-03-10 21:32:32 +0300

    правки по авторизации через аутлук

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const User = require('../models').User;
 
-router.post('/login', require('./login'));
-router.get('/user/me', (req, res) => {
+router.post('/login', require('./login')); // router = /api/login  (общая точка логина для всех способов)
+router.get('/user/me', (req, res) => {     // router = /api/user/me
   User.findOne({googleId: req.decoded.user}, (err, user) => {
     res.send(user);
   })
