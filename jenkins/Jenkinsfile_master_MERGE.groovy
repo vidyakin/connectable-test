4e2b562622031b221fefe3563349d08ef10edcf2
yanp <yanp@example.com>
2020-03-21 19:31:29 +0200

    master Merge pipeline WIP (creds)

--- jenkins/Jenkinsfile_master_MERGE.groovy
+++ jenkins/Jenkinsfile_master_MERGE.groovy
@@ -1,70 +1,111 @@
 pipeline {
-    agent { 
+    agent {
         label 'master'
     }
     environment {
-        GIT_REPO = 'https://github.com/connectable-project/connectable.git'
+        GIT_REPO_PATH = 'connectable-project/connectable'
         GIT_CREDENTIALS = 'github_conn_http'
-        GIT_BRANCH = 'test_ssh'
+        GIT_BRANCH = 'new_master'
         SSH_PROD_CREDENTIALS = 'ssh_conn_prod'
         SSH_PROD_IP = '10.128.10.0'
         PROD_ROOT_DIR = 'connectable'
+        PROD_ENV_FILE = '.env.prod'
     }
     stages {
-        stage("Git Checkout") {
+        stage("Prepare PROD ENV") {
             steps {
-                cleanWs()
                 script {
-                   echo "${GIT_BRANCH}"
-                   def scmVars = checkout(
-                        scm: [
-                            $class: "GitSCM",
-                            branches: [[name: "${GIT_BRANCH}"]],
-                            userRemoteConfigs: [[
-                                credentialsId: "${GIT_CREDENTIALS}",
-                                url: "${GIT_REPO}"
-                            ]]
-                        ]
-                    )
+                withCredentials([sshUserPrivateKey(
+                                   credentialsId: "${SSH_PROD_CREDENTIALS}",
+                                   keyFileVariable: 'SSH_KEYFILE',
+                                   usernameVariable: 'SSH_USERNAME'
+                                 ),
+                                 usernamePassword(
+                                    credentialsId: 'mongodb_admin',
+                                    passwordVariable: 'MONGO_INITDB_ROOT_PASSWORD',
+                                    usernameVariable: 'MONGO_INITDB_ROOT_USERNAME'
+                                 ),
+                                 usernamePassword(
+                                    credentialsId: "mongodb_app",
+                                    passwordVariable: 'MONGO_PASS',
+                                    usernameVariable: 'MONGO_USER'
+                                 )
+                ]) {
+                     NEW_RELEASE_DIR = "${PROD_ROOT_DIR}-${BUILD_NUMBER}"
+                     OLD_RELEASE_DIRS = sh(
+                        script: """
+                                ssh -i ${SSH_KEYFILE} -o StrictHostKeyChecking=no ${SSH_USERNAME}@${SSH_PROD_IP} \
+                                '
+                                OLD_RELEASE_DIRS=\$(basename \$HOME}/${PROD_ROOT_DIR}-*) &&
+                                echo \$OLD_RELEASE_DIRS
+                                '
+                               """,
+                        returnStdout: true
+                        ).trim()
+                    sh """cat >> ${PROD_ENV_FILE} <<OUT
+                          MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME}
+                          MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_PASSWORD}
+                          MONGO_USER=${MONGO_USER}
+                          MONGO_PASS=${MONGO_PASS}
+                          DB_URL=mongodb://${MONGO_USER}:${MONGO_PASS}@mongodb:27017/connectable
+                          OUT
+                        """
+                    sh "scp -i ${SSH_KEYFILE} ${PROD_ENV_FILE} ${SSH_USERNAME}@${SSH_PROD_IP}:~/${PROD_ENV_FILE}"
+                    }
                 }
             }
         }
-        stage("Check CREDS") {
+        stage("Fetch updates to PROD") {
             steps {
-                withCredentials([usernamePassword(credentialsId: "${GIT_CREDENTIALS}",
-                                                 passwordVariable: 'GIT_PASS',
-                                                 usernameVariable: 'GIT_USER'),
-                ]) {
-                sh """echo ${GIT_USER} >> creds.out
-                """
-                }
-            }
-        }
-        stage("Check SSH") {
-            steps {
-                withCredentials([sshUserPrivateKey(credentialsId: "${SSH_PROD_CREDENTIALS}", 
-                                                   keyFileVariable: 'SSH_KEYFILE',
-                                                   usernameVariable: 'SSH_USERNAME'),
-                                usernamePassword(credentialsId: "${GIT_CREDENTIALS}", 
-                                                 passwordVariable: 'GIT_PASS',
-                                                 usernameVariable: 'GIT_USER'),
-                ]) {
+                withCredentials([sshUserPrivateKey(
+                                   credentialsId: "${SSH_PROD_CREDENTIALS}",
+                                   keyFileVariable: 'SSH_KEYFILE',
+                                   usernameVariable: 'SSH_USERNAME'
+                                 ),
+                                 usernamePassword(
+                                    credentialsId: "${GIT_CREDENTIALS}",
+                                    passwordVariable: 'GIT_PASS',
+                                    usernameVariable: 'GIT_USER'
+                                 )]) {
                 sh """ssh -i ${SSH_KEYFILE} -o StrictHostKeyChecking=no ${SSH_USERNAME}@${SSH_PROD_IP} \
-                OLD_RELEASE=\$(basename /home/${SSH_USERNAME}/${PROD_ROOT_DIR}-*) && \
-                echo \$OLD_RELEASE && \
-                rm -rf \$OLD_RELEASE && \
-                \'ls /home/${SSH_USERNAME} && \
-                echo 'WUT' && \
-                ls /home/${SSH_USERNAME}\'
+                '
+                mkdir \$HOME/${NEW_RELEASE_DIR} &&
+                git clone https://${GIT_USER}:${GIT_PASS}@github.com/${GIT_REPO_PATH} \$HOME/${NEW_RELEASE_DIR} &&
+                cat \$HOME/${PROD_ENV_FILE} >> \$HOME/${NEW_RELEASE_DIR}/${}
+                '
                 """
                 }
             }
         }
+        // stage("Check SSH") {
+        //     steps {
+        //         withCredentials([sshUserPrivateKey(
+        //                            credentialsId: "${SSH_PROD_CREDENTIALS}",
+        //                            keyFileVariable: 'SSH_KEYFILE',
+        //                            usernameVariable: 'SSH_USERNAME'
+        //                          ),
+        //                          usernamePassword(
+        //                             credentialsId: "${GIT_CREDENTIALS}",
+        //                             passwordVariable: 'GIT_PASS',
+        //                             usernameVariable: 'GIT_USER'
+        //                          ),
+        //                          file(
+        //                             credentialsId: "${PROD_ENV_FILE}",
+        //                             variable: 'PROD_ENV_SECRETS'
+        //                          )]) {
+        //         sh """ssh -i ${SSH_KEYFILE} -o StrictHostKeyChecking=no ${SSH_USERNAME}@${SSH_PROD_IP} \
+        //         '
+        //         NEW_RELEASE=${PROD_ROOT_DIR}-${BUILD_NUMBER} &&
+        //         cd \$HOME/\$NEW_RELEASE &&
+        //         docker-compose -f docker-compose-prod.yaml build connfrontend &&
+        //         docker-compose -f docker-compose-prod.yaml up --no-deps -d connfrontend &&
+        //         docker-compose -f docker-compose-prod.yaml build connbackend &&
+        //         docker-compose -f docker-compose-prod.yaml up --no-deps -d connbackend &&
+        //         test -d \"\$HOME\$OLD_RELEASE\" && rm -rf \"\$HOME\$OLD_RELEASE\" || echo "No dir: \$OLD_RELEASE\"
+        //         '
+        //         """
+        //         }
+        //     }
+        // }
     }
 }
-ssh serverA "bash -s" < ./ex.bash
-
-                OLD_RELEASE=\$(basename /home/${SSH_USERNAME}/${PROD_ROOT_DIR}-*) && \
-                echo \$OLD_RELEASE && \
-                rm -rf \$OLD_RELEASE && \
-                echo \$hostname && \
