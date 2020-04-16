2964db50e3310ea8235efbf44d1fab0f5b36c3b0
yanp <yanp@example.com>
2020-04-16 10:47:55 +0300

    no cache when building

--- jenkins/Jenkinsfile_PR.groovy
+++ jenkins/Jenkinsfile_PR.groovy
@@ -26,12 +26,12 @@ pipeline {
         }
         stage("Build Frontend") {
             steps {
-                    sh "docker-compose build connfrontend"
+                    sh "docker-compose build --force-rm --no-cache connfrontend"
             }
         }
         stage("Build Backend") {
             steps {
-                    sh "docker-compose build connbackend"
+                    sh "docker-compose build --force-rm --no-cache connbackend"
             }
         }
     }
