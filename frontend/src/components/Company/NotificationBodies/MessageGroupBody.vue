249001b3553d8dc81b111dd4d6139d95323b71ff
vidyakin <vidyakin.sergey@gmail.com>
2020-08-03 17:53:26 +0300

    fix: correct group name in notification list

--- frontend/src/components/Company/NotificationBodies/MessageGroupBody.vue
+++ frontend/src/components/Company/NotificationBodies/MessageGroupBody.vue
@@ -2,15 +2,23 @@
   <div>
     <b>{{creator}}</b> создал новую группу:
     <i>
-      <router-link :to="'/group/'+id">{{name}}</router-link>
+      <router-link :to="'/group/'+id">{{getGroupName}}</router-link>
     </i>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 export default {
   name: "MsgGroupBody",
-  props: ["id", "creator", "name"]
+  props: ["id", "creator", "name"],
+  computed: {
+    ...mapGetters(["groups"]),
+    getGroupName() {
+      const group = this.groups.find((g) => g._id == this.id);
+      return group ? group.name : `not found (id ${this.id})`;
+    },
+  },
 };
 </script>
 
